d5b8761d58c80b5830a281f13533405e0f1ba91a
johan <johan.hertz@hotmail.com>
2026-09-13 19:12:51 +0200

    colorscheme

--- hub-web/src/main.tsx
+++ hub-web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme, type MantineColorsTuple } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import "@mantine/core/styles.css";
@@ -8,9 +8,29 @@ import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
 import App from "./App.tsx";
 
+const myColor: MantineColorsTuple = [
+  '#f1f1ff',
+  '#e0dff2',
+  '#bfbdde',
+  '#9b98ca',
+  '#7d79b9',
+  '#6a66af',
+  '#605cac',
+  '#504c97',
+  '#464388',
+  '#3b3979'
+];
+
+const myTheme = createTheme({
+  colors: {
+    myColor,
+  },
+  primaryColor: 'myColor',
+}); 
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider defaultColorScheme="dark">
+    <MantineProvider theme={myTheme} defaultColorScheme="dark">
       <QueryClientProvider client={new QueryClient()}>
         <BrowserRouter>
           <App />
